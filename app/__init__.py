1080af3bab1064e6f9633d12b1bf495a502997c3
Teddie <teddymachoka98@yahoo.com>
2020-09-12 22:52:01 +0300

    added content to the main init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
-from flask import Blueprint
-main = Blueprint('main',__name__)
-from . import views,error
+from flask import Flask
+from config import config_options
+
+# Initializing application
+def create_app(config_name):
+    app = Flask(__name__)
